435e60fd1b6948faacca86bcefb550918b8ae19b
asranet <info@asranet.co.uk>
2016-06-14 09:03:03 +0100

    Fixed width issue on mobile devices

--- mainApp/views.py
+++ mainApp/views.py
@@ -11,6 +11,10 @@ def index(request):
     return render(request, 'index.html', {"story_list": story_list, "title": "Home", "site_settings": site_settings})
 
 
+def index2(request):
+    return render(request, 'index2.html')
+
+
 def venue(request):
     story_list = Item.objects.filter(page__name="venue").order_by('order')
     site_settings = SiteSetting.objects.all().first()
